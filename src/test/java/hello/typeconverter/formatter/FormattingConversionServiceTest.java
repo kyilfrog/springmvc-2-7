77644e66b26e1b37e20a47ed9e6870efb88f53a2
kyilfrog <160857010+kyilfrog@users.noreply.github.com>
2024-06-05 20:34:30 +0900

    포맷터를 지원하는 컨버전 서비스

---
+++ src/test/java/hello/typeconverter/formatter/FormattingConversionServiceTest.java
@@ -0,0 +1,63 @@
+package hello.typeconverter.formatter;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.format.support.DefaultFormattingConversionService;
+
+import hello.typeconverter.converter.IpPortToStringConverter;
+import hello.typeconverter.converter.StringToIpPortConverter;
+import hello.typeconverter.type.IpPort;
+
+public class FormattingConversionServiceTest {
+
+	@Test
+	void formattingConversionService() {
+		DefaultFormattingConversionService conversionService = new DefaultFormattingConversionService();
+		
+		//컨버터 등록
+		conversionService.addConverter(new StringToIpPortConverter());
+		conversionService.addConverter(new IpPortToStringConverter());
+		//포맷터 등록
+		conversionService.addFormatter(new MyNumberFormatter());
+		
+		//컨버터 사용
+		IpPort ipPort = conversionService.convert("127.0.0.1:8080", IpPort.class);
+		Assertions.assertThat(ipPort).isEqualTo(new IpPort("127.0.0.1", 8080));
+		//포멧터 사용
+		String convert = conversionService.convert(1000, String.class);
+		Assertions.assertThat(convert).isEqualTo("1,000");
+		
+		Long convert2 = conversionService.convert("1,000", Long.class);
+		Assertions.assertThat(convert2).isEqualTo(1000L);
+
+
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
